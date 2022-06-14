1b44922c3fbbc96679b79669d90b2a5a081f91d0
HNO234 <dave910602@gmail.com>
2022-06-14 16:50:42 +0800

    remove comments in show.py

--- show.py
+++ show.py
@@ -24,25 +24,3 @@ with open(file_path, newline='') as csvfile:
         table[i // 5][i % 5] = round(agent_list[i][1], 2)
     for i in range(5):
         print(*table[i])
-
-
-# for i in range(5):
-#     for j in range(5):
-#         file_path = f'./Result/{i+1}_{j+1}.txt'
-#         with open(file_path, newline='') as csvfile:
-#             rows = list(csv.reader(csvfile))
-#             agents = rows[0]
-#             win = rows[1:]
-#
-#             best_win_rate, best_model = 0.0, ''
-#             for k in range(6):
-#                 win_rate = 0.0
-#                 for l in range(6):
-#                     if k != l:
-#                         win_rate += float(win[k][l])
-#                 win_rate /= 5
-#                 if win_rate >= best_win_rate:
-#                     best_win_rate = win_rate
-#                     best_model = agents[k]
-#
-#             print(agent_place[j], best_model, algo[j])
